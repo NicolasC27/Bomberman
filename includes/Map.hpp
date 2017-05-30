9ea8362f49c1d26476b50ffdf25e6a1c4bcbf4ad
nicolas <nicolas1.chevalier@epitech.eu>
2017-05-31 00:02:21 +0200

    class map

--- includes/Map.hpp
+++ includes/Map.hpp
@@ -1,33 +1,45 @@
 //
-// Created by root on 29/05/17.
+// Created by nicolas on 29/05/17.
 //
 
 #ifndef CPP_INDIE_STUDIO_MAP_HPP
 #define CPP_INDIE_STUDIO_MAP_HPP
 
 #include <OgreVector2.h>
-#include "AGameObject.hpp"
 #include <OgreException.h>
+#include <map>
+#include "AGameObject.hpp"
 
-#define ERR_MAPSIZE 	"Size map too big"
-#define NOT_FOUND   	"File not found"
+#define ERR_MAPSIZE	"Size map too big"
+#define ERR_NBLINEMAP	"The number of rows does not match the number of rows in the map."
+#define NOT_FOUND	"File not found"
 #define FOLDER_MAP	"media/map"
 
-class Map {
+class Map
+{
 
  public:
   Map(std::string const &filename = "media/map/map1");
-
   virtual ~Map();
 
-  void generate();
 
  private:
-  std::map<Ogre::Vector2, AGameObject *> 	_objects;
-  const std::string 				_filename;
-  int					_size;
+  std::map<AGameObject *, Ogre::Vector2> 	_objects;
+  std::map<AGameObject *, Ogre::Vector2> 	_character;
  public:
-  void setSize(int size);
+  void 	addCharacter(const Ogre::Vector2 &vector, AGameObject *character);
+
+ private:
+  std::list<Ogre::Vector2>			_spawns;
+  std::string	 				_filename;
+  int 						_size;
+
+ public:
+  void	generateObjects();
+  void	generateSpawn();
+  void	setSize(int size);
+  void 	addObjects(const Ogre::Vector2 &, AGameObject *);
+
 };
 
 #endif //CPP_INDIE_STUDIO_MAP_HPP
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,14 +1,19 @@
 //
-// Created by root on 29/05/17.
+// Created by nicolas on 29/05/17.
 //
 
 #include "Map.hpp"
+#include "Wall.hpp"
 #include <iostream>
 #include <fstream>
 
 Map::Map(std::string const &filename) : _filename(filename)
 {
+  std::ifstream infile(filename);
 
+  if (!(infile.good()))
+    throw Ogre::Exception(Ogre::Exception::ERR_FILE_NOT_FOUND,
+			  NOT_FOUND, FOLDER_MAP);
 }
 
 Map::~Map()
@@ -16,24 +21,50 @@ Map::~Map()
 
 }
 
-void Map::generate()
+void Map::addObjects(const Ogre::Vector2 &vector, AGameObject *object)
+{
+  _objects.insert(std::pair<AGameObject *, Ogre::Vector2>(object, vector));
+}
+
+void 		Map::generateObjects()
 {
   std::ifstream infile(_filename);
-  std::string line;
-  int size;
+  std::string 	line;
+  std::string	digit;
+  int 		count;
 
-  if (!(infile.good()))
-    throw Ogre::Exception(Ogre::Exception::ERR_FILE_NOT_FOUND,
-			  NOT_FOUND, FOLDER_MAP);
   std::getline(infile, line);
   setSize(std::stoi(line));
   if (_size > 40)
     throw Ogre::Exception(Ogre::Exception::ERR_INVALID_STATE,
 			  ERR_MAPSIZE, _filename);
-  while (std::getline(infile, line))
-    {
-	
-    }
+  for (int y = 0; std::getline(infile, line); y++)
+  {
+      for(int x = 0, i = 0; line[i]; i++, x++)
+	{
+	  if ((line[i] - '0') == AGameObject::WALL)
+	    addObjects(Ogre::Vector2(x, y), new Wall(Wall::UNBREAKABLE));
+	  else if ((line[i] - '0')  == AGameObject::BLOCK)
+	      addObjects(Ogre::Vector2(x, y), new Wall(Wall::BREAKABLE));
+	}
+      count = y;
+  }
+  if (count != (_size - 1))
+    throw Ogre::Exception(Ogre::Exception::ERR_INVALID_STATE,
+			  ERR_NBLINEMAP, _filename);
+}
+
+void 		Map::generateSpawn()
+{
+  _spawns.push_front(Ogre::Vector2(0, 0));
+  _spawns.push_front(Ogre::Vector2(0, _size - 1));
+  _spawns.push_front(Ogre::Vector2(_size - 1, 0));
+  _spawns.push_front(Ogre::Vector2(_size - 1, _size - 1));
+}
+
+void Map::addCharacter(const Ogre::Vector2 &vector, AGameObject *character)
+{
+  _character.insert(std::pair<AGameObject *, Ogre::Vector2>(character, vector));
 }
 
 void Map::setSize(int size)
