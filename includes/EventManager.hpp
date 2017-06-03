6c124f73b91b361d9646896e76960fa7d10b79d5
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-03 19:12:52 +0200

    update eventmanager

--- includes/EventManager.hpp
+++ includes/EventManager.hpp
@@ -6,36 +6,32 @@
 #define BOMBERMAN_EVENTMANAGER_HPP
 
 #include <OgreFrameListener.h>
+#include "InputManager.hpp"
 #include <OgreException.h>
 #include <OIS/OIS.h>
 #include <Ogre.h>
 
-class EventManager : public Ogre::FrameListener, public OIS::JoyStickListener
+class EventManager : public Ogre::FrameListener,  public Ogre::WindowEventListener
 {
- public:
-  EventManager();
-  ~EventManager();
-
-  bool capture(const Ogre::FrameEvent &evt);
-  void initialize(Ogre::SceneNode *, Ogre::RenderWindow *, Ogre::Camera *);
-
-  bool axisMoved(const OIS::JoyStickEvent &e, int axis);
-  bool sliderMoved(const OIS::JoyStickEvent &e, int sliderID);
-  bool buttonPressed(const OIS::JoyStickEvent &e, int button);
-  bool buttonReleased(const OIS::JoyStickEvent &e, int button);
-  bool vector3Moved(const OIS::JoyStickEvent &arg, int index);
-  bool povMoved(const OIS::JoyStickEvent &arg, int index);
-
  private:
+  Ogre::RenderWindow 	*mWindow;
   OIS::InputManager 	*mInputManager;
   OIS::Keyboard 	*mKeyboard;
   OIS::Mouse 		*mMouse;
-  OIS::JoyStick 	*mJoystick;
-  Ogre::SceneNode 	*mNode;
   Ogre::Camera 		*mCamera;
+  float mMovementspeed;
 
-  float 		mMovementspeed;
+ public:
+  EventManager(Ogre::RenderWindow *, Ogre::Camera *);
+  ~EventManager();
 
+  void windowClosed(Ogre::RenderWindow *windows);
+  void windowResized(Ogre::RenderWindow *wnd);
+
+  bool frameStarted(const Ogre::FrameEvent &evt);
+  bool frameEnded(const Ogre::FrameEvent &evt);
+  bool frameRenderingQueued(const Ogre::FrameEvent &evt);
 };
+
 
 #endif //BOMBERMAN_EVENTMANAGER_HPP
--- src/Controller/EventManager.cpp
+++ src/Controller/EventManager.cpp
@@ -1,151 +1,114 @@
-//
 // Created by nicolas on 01/06/17.
-//
+
 
 #include "EventManager.hpp"
 
-
-EventManager::EventManager()
+EventManager::EventManager(Ogre::RenderWindow *Window, Ogre::Camera *camera)
 {
+  Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
 
-}
-
-EventManager::~EventManager()
-{
-  mInputManager->destroyInputObject(mKeyboard);
-  mInputManager->destroyInputObject(mMouse);
-  OIS::InputManager::destroyInputSystem(mInputManager);
-}
-
-void EventManager::initialize(Ogre::SceneNode *node, Ogre::RenderWindow *mWindow,
-			      Ogre::Camera *camera)
-{
-  size_t windowHnd;
-  std::stringstream windowHndStr;
   OIS::ParamList pl;
+  std::stringstream windowHndStr;
+  size_t windowHnd = 0;
 
-  windowHnd = 0;
-  mNode = node;
+  mWindow = Window;
   mCamera = camera;
   mMovementspeed = 5.0f;
-
-  Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
   mWindow->getCustomAttribute("WINDOW", &windowHnd);
+
   windowHndStr << windowHnd;
 
   pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
   mInputManager = OIS::InputManager::createInputSystem(pl);
-  mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, true ));
-  mMouse = static_cast<OIS::Mouse *>(mInputManager->createInputObject(OIS::OISMouse, true));
-//  if (mInputManager->getNumberOfDevices(OIS::OISJoyStick) > 0)
-//    {
-//      mJoystick = static_cast<OIS::JoyStick*>( mInputManager->createInputObject( OIS::OISJoyStick, true ) );
-//      mJoystick = static_cast<OIS::JoyStick*>( mInputManager->createInputObject( OIS::OISJoyStick, true ) );
-//      mJoystick = static_cast<OIS::JoyStick*>( mInputManager->createInputObject( OIS::OISJoyStick, true ) );
-//      mJoystick->setEventCallback(this);
-//    }
+
+  mKeyboard = static_cast<OIS::Keyboard *>(mInputManager->createInputObject(
+	  OIS::OISKeyboard, true));
+  mMouse = static_cast<OIS::Mouse *>(mInputManager->createInputObject(
+	  OIS::OISMouse, true));
+
+  windowResized(mWindow);
+  Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
 }
 
-bool EventManager::capture(const Ogre::FrameEvent &evt)
+EventManager::~EventManager()
 {
-  Ogre::Vector3 translate(0,0,0);
 
+  mInputManager->destroyInputObject(mKeyboard);
+  mInputManager->destroyInputObject(mMouse);
+
+  OIS::InputManager::destroyInputSystem(mInputManager);
+  Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, this);
+  windowClosed(mWindow);
+}
+
+void EventManager::windowClosed(Ogre::RenderWindow *windows)
+{
+  if (windows == mWindow)
+    {
+      if (mInputManager)
+	{
+	  mInputManager->destroyInputObject(mMouse);
+	  mInputManager->destroyInputObject(mKeyboard);
+
+	  OIS::InputManager::destroyInputSystem(mInputManager);
+	  mInputManager = 0;
+	}
+    }
+}
+
+void EventManager::windowResized(Ogre::RenderWindow *windows)
+{
+  unsigned int width, height, depth;
+  int left, top;
+
+  windows->getMetrics(width, height, depth, left, top);
+  const OIS::MouseState &ms = mMouse->getMouseState();
+  ms.width = width;
+  ms.height = height;
+}
+
+bool EventManager::frameStarted(const Ogre::FrameEvent &evt)
+{
+  return true;
+}
+
+
+bool EventManager::frameEnded(const Ogre::FrameEvent &evt)
+{
+  return true;
+}
+
+
+bool EventManager::frameRenderingQueued(const Ogre::FrameEvent &evt)
+{
+  Ogre::Vector3 translate(0, 0, 0);
+
+  if (mWindow->isClosed())
+    return false;
   mKeyboard->capture();
   mMouse->capture();
-  mJoystick->capture();
 
-  if(mKeyboard->isKeyDown(OIS::KC_ESCAPE))
-    {
-      return false;
-    }
 
-  if(mKeyboard->isKeyDown(OIS::KC_W))
-    {
-      translate += Ogre::Vector3(0,0,-10);
-    }
+  if (mKeyboard->isKeyDown(OIS::KC_ESCAPE))
+    return false;
 
-  if(mKeyboard->isKeyDown(OIS::KC_S))
-    {
-      translate += Ogre::Vector3(0,0,10);
-    }
+  if (mKeyboard->isKeyDown(OIS::KC_W))
+    translate += Ogre::Vector3(0, 0, -10);
 
-  if(mKeyboard->isKeyDown(OIS::KC_A))
-    {
-      translate += Ogre::Vector3(-10,0,0);
-    }
+  if (mKeyboard->isKeyDown(OIS::KC_S))
+    translate += Ogre::Vector3(0, 0, 10);
 
-  if(mKeyboard->isKeyDown(OIS::KC_D))
-    {
-      translate += Ogre::Vector3(10,0,0);
-    }
+  if (mKeyboard->isKeyDown(OIS::KC_A))
+    translate += Ogre::Vector3(-10, 0, 0);
 
+  if (mKeyboard->isKeyDown(OIS::KC_D))
+    translate += Ogre::Vector3(10, 0, 0);
+
+  float rotX = mMouse->getMouseState().X.rel * evt.timeSinceLastFrame * -1;
   float rotY = mMouse->getMouseState().Y.rel * evt.timeSinceLastFrame * -1;
-  float rotX = mMouse->getMouseState().X.rel * evt.timeSinceLastFrame* -1;
-
   mCamera->yaw(Ogre::Radian(rotX));
   mCamera->pitch(Ogre::Radian(rotY));
 
   mCamera->moveRelative(translate * evt.timeSinceLastFrame * mMovementspeed);
-
-  return true;
-}
-
-
-bool EventManager::axisMoved(const OIS::JoyStickEvent &e, int axis)
-{
-  if( e.state.mAxes[axis].abs > 2500 || e.state.mAxes[axis].abs < -2500 )
-    std::cout << std::endl << e.device->vendor() << ". Axis # " << axis << " Value: " << e.state.mAxes[axis].abs;
-}
-
-bool EventManager::buttonPressed(const OIS::JoyStickEvent &e, int button)
-{
-  std::cout << "buttonPressed : " <<  button << std::endl;
-  return true;
-}
-
-bool EventManager::buttonReleased(const OIS::JoyStickEvent &e, int button)
-{
-  std::cout << "buttonReleased : " <<  button << std::endl;
-
-  return true;
-}
-
-bool EventManager::sliderMoved(const OIS::JoyStickEvent &e, int sliderID)
-{
-  std::cout << "sliderMoved : " <<  sliderID << std::endl;
-
-  return true;
-}
-
-bool EventManager::vector3Moved(const OIS::JoyStickEvent &arg, int index)
-{
-  return true;
-}
-
-bool EventManager::povMoved(const OIS::JoyStickEvent &arg, int index)
-{
-  Ogre::Vector3 translate(0,0,0);
-
-
-  if (arg.state.mPOV[index].direction == arg.state.mPOV->North)
-    {
-      translate += Ogre::Vector3(0, 0, -10);
-
-    } else
-    if (arg.state.mPOV[index].direction == arg.state.mPOV->South)
-      {
-	translate += Ogre::Vector3(0, 0, 10);
-      } else
-      if (arg.state.mPOV[index].direction == arg.state.mPOV->East)
-	{
-	  translate += Ogre::Vector3(10, 0, 0);
-	} else
-	if (arg.state.mPOV[index].direction == arg.state.mPOV->West)
-	  {
-	    translate += Ogre::Vector3(-10, 0, 0);
-	  }
-
-  mCamera->moveRelative(translate * 0.5);
-
   return true;
 }
