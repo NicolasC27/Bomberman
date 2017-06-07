1f07f41951b338fd2327076e294ae3407bb7556a
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-07 02:32:42 +0200

    update map and create plan

--- includes/Map.hpp
+++ includes/Map.hpp
@@ -8,9 +8,10 @@
 #include <OgreVector2.h>
 #include <OgreException.h>
 #include <map>
-#include "Controller/GameManager.hpp"
-#include "Controller/NodeManager.hpp"
-#include "AGameObject.hpp"
+
+#include "Objects/AGameObject.hpp"
+#include "Common/Manager/GameManager.hpp"
+#include "Common/Manager/NodeManager.hpp"
 
 #define ERR_MAPSIZE	"Size map too big"
 #define ERR_NBLINEMAP	"The number of rows does not match the number of rows in the map."
@@ -20,29 +21,36 @@
 class Map
 {
  private:
-  static const int				boxWidth = 100;
+  static const int 				boxWidth = 100;
 
-  Controller::NodeManager			*_nodes;
-  Ogre::SceneManager				*_SceneManager;
+  NodeManager 					*_nodes;
+  Ogre::SceneManager 				*_SceneManager;
 
   std::multimap<Ogre::Vector2, AGameObject *> 	_objects;
   std::multimap<AGameObject *, Ogre::Vector2> 	_character;
-  std::list<Ogre::Vector2>			_spawns;
-  std::string	 				_filename;
+
+  std::list<Ogre::Vector2> 			_spawns;
+  std::string 					_filename;
+
   int 						_size;
 
  public:
 
-  Map(std::string const &filename = "media/map/map1", Ogre::SceneManager *SceneManager = NULL, Controller::NodeManager *node = NULL);
+  Map(std::string const &filename = "media/map/map1",
+      Ogre::SceneManager *SceneManager = NULL,
+      NodeManager *node = NULL);
+
   virtual ~Map();
 
-  void	generateObjects();
-  void	generateSpawn();
+  void generateObjects();
+  void generateSpawn();
+  void generatePlan();
 
-  void	setSize(int size);
+  void setSize(int size);
+  int getSize() const;
 
-  void 	addObjects(const Ogre::Vector2 &, AGameObject *);
-  void 	addCharacter(const Ogre::Vector2 &vector, AGameObject *character);
+  void addObjects(const Ogre::Vector2 &, AGameObject *);
+  void addCharacter(const Ogre::Vector2 &vector, AGameObject *character);
 };
 
 #endif //CPP_INDIE_STUDIO_MAP_HPP
