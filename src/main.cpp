cce7ba0bfa0fc72bf77f12b4cbc00407fab74a5e
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-07 20:57:18 +0200

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,7 @@
 
 #include "Common/Manager/GameManager.hpp"
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-#define WIN32_LEAN_AND_MEAN
-#include "windows.h"
-
-
-INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
-#else
 int main(int argc, char **argv)
-#endif
 {
   GameManager app;
 
@@ -22,11 +14,7 @@ int main(int argc, char **argv)
       app.run();
     }
   catch (Ogre::Exception &e) {
-	#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-      		MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
-	#else
-      		std::cerr << "An exception has occured: " << e.getFullDescription();
-	#endif
+      std::cerr << "An exception has occured: " << e.getFullDescription();
     }
   return 0;
 }
