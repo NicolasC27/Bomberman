dac137bc9dd5c89c8d5e1d93d7c8b59aaa3ccc32
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-07 02:32:35 +0200

    update map and create plan

--- src/Map.cpp
+++ src/Map.cpp
@@ -3,11 +3,12 @@
 //
 
 #include "Map.hpp"
-#include "Wall.hpp"
+#include "Objects/Ground.hpp"
+#include "Objects/Wall.hpp"
 
 
 Map::Map(std::string const &filename, Ogre::SceneManager *SceneManager,
-	 Controller::NodeManager *node) : _filename(filename), _SceneManager(SceneManager), _nodes(node)
+	 NodeManager *node) : _filename(filename), _SceneManager(SceneManager), _nodes(node)
 {
   std::ifstream infile(filename);
 
@@ -26,9 +27,30 @@ void 		Map::addObjects(const Ogre::Vector2 &vector, AGameObject *object)
   _objects.insert(std::pair<Ogre::Vector2, AGameObject *>(vector, object));
   object->setSceneManager(_SceneManager);
   object->createEntity();
-  object->setPosition(Map::boxWidth * (vector.x), 0, Map::boxWidth * vector.y);
+  object->setPosition(Map::boxWidth * (vector.x), object->getPositionY(), Map::boxWidth * vector.y);
   object->AttachObject();
+}
 
+void		Map::generatePlan()
+{
+  Ogre::Plane 		plane(Ogre::Vector3::UNIT_Y, 0);
+  Ogre::Entity		*plan;
+
+  Ogre::MeshManager::getSingleton().createPlane(
+	  "ground",
+	  Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+	  plane,
+	  _size * 100, _size * 100, 20, 20,
+	  true,
+	  1, 5, 5,
+	  Ogre::Vector3::UNIT_Z);
+
+  plan = _SceneManager->createEntity("ground");
+  plan->setMaterialName ("Objects/Ground/BumpyMetal");
+
+  Ogre::SceneNode *node = _SceneManager->getRootSceneNode()->createChildSceneNode();
+  node->setPosition((_size * 100) / 2 - 50, -50, (_size * 100) / 2 - 55);
+  node->attachObject(plan);
 }
 
 void 		Map::generateObjects()
@@ -45,21 +67,21 @@ void 		Map::generateObjects()
 			  ERR_MAPSIZE, _filename);
   for (int x = 0; std::getline(infile, line); x++)
   {
-      double xPos = Map::boxWidth * x;
       for(int z = 0, i = 0; line[i]; i++, z++)
 	{
-	  double zPos = Map::boxWidth * z;
 	  if ((line[i] - '0') == AGameObject::WALL)
 	    addObjects(Ogre::Vector2(z, x), new Wall(Wall::UNBREAKABLE));
 	  else if ((line[i] - '0')  == AGameObject::BLOCK)
-	      addObjects(Ogre::Vector2(z, x), new Wall(Wall::BREAKABLE)); }
+	      addObjects(Ogre::Vector2(z, x), new Wall(Wall::BREAKABLE));
+//	  else
+//	      addObjects(Ogre::Vector2(z, x), new Ground(Ground::UNBREAKABLE));
+	}
       count = x;
   }
-//  if (count != (_size - 1))
-//    throw Ogre::Exception(Ogre::Exception::ERR_INVALID_STATE,
-//			  ERR_NBLINEMAP, _filename);
-
-
+  if (count != (_size - 1))
+    throw Ogre::Exception(Ogre::Exception::ERR_INVALID_STATE,
+			  ERR_NBLINEMAP, _filename);
+  generatePlan();
 }
 
 void 		Map::generateSpawn()
@@ -78,4 +100,9 @@ void 		Map::addCharacter(const Ogre::Vector2 &vector, AGameObject *character)
 void 		Map::setSize(int size)
 {
   _size = size;
+}
+
+int 		Map::getSize() const
+{
+  return _size;
 }
