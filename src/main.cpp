b9956b53da754a341ded9d102d81d969e2967aab
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-10 20:48:45 +0200

    compatibility windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 // Created by nicolas on 29/05/17.
 //
 
-
 #include "Common/Manager/GameManager.hpp"
 
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
+
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
 int main(int argc, char **argv)
+#endif
 {
   GameManager app;
 
@@ -14,7 +21,11 @@ int main(int argc, char **argv)
       app.run();
     }
   catch (Ogre::Exception &e) {
-      std::cerr << "An exception has occured: " << e.getFullDescription();
+	#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+      		MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+	#else
+      		std::cerr << "An exception has occured: " << e.getFullDescription();
+	#endif
     }
   return 0;
 }
