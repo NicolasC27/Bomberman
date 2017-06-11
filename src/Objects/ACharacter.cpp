5d59ceb48937a204bf1eb1621ff9a2b1d65535bc
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-12 01:29:09 +0200

    test move and collision

--- src/Objects/ACharacter.cpp
+++ src/Objects/ACharacter.cpp
@@ -42,7 +42,7 @@ std::string ACharacter::getMaterialName() const
 Ogre::Vector3 ACharacter::getScale() const
 {
 
-  return Ogre::Vector3(2, 2,2);
+  return Ogre::Vector3(0.8, 0.8, 0.8);
 }
 
 double ACharacter::getPositionY() const
@@ -66,31 +66,42 @@ void ACharacter::move(Ogre::Vector3 const &vector, const Ogre::FrameEvent &evt)
   static Ogre::AnimationState *mAnimationState;
 
   mAnimationState = dynamic_cast<Ogre::Entity*>(_obj)->getAnimationState("my_animation");
+
+  Ogre::Real mMoveSpeed  = 400;
+  Ogre::Vector3 translateVector = 	vector;
+
+  Collision *test;
+
+  test = new Collision((Ogre::Vector2(100, 100)));
+
+  test->setOrigin(Ogre::Vector2(100, 100));
   mAnimationState->setLoop(true);
   mAnimationState->setEnabled(true);
 
-  Ogre::Real mMoveSpeed  =400;
-  Ogre::Vector3 translateVector = evt.timeSinceLastFrame * mMoveSpeed * vector;
-
   _node->translate(translateVector);
-
   //Rotate the object to the moving direction
 
-  if(translateVector != Ogre::Vector3::ZERO) {
+  _collision->setOrigin(Ogre::Vector2(_node->getPosition().x, _node->getPosition().z));
+  if (_collision == test)
+    std::cout << "COLLISION" << std::endl;
+  if (translateVector != Ogre::Vector3::ZERO)
+    {
 
       Ogre::Vector3 src = _node->getOrientation() * Ogre::Vector3::UNIT_Z;
       Ogre::Vector3 mDirection = vector;
       mDirection.normalise();
 
-      if ((1.0f + src.dotProduct(mDirection)) < 0.0001f) {
+      if ((1.0f + src.dotProduct(mDirection)) < 0.0001f)
+	{
 	  _node->yaw(Ogre::Degree(180));
-	} else {
+	} else
+	{
 	  Ogre::Quaternion quat = src.getRotationTo(mDirection);
 	  _node->rotate(quat);
 
 	} // else
     }
   mAnimationState->addTime(evt.timeSinceLastFrame * 1.5);
-
   std::cout << "x : " << _node->getPosition().x << "z: " << _node->getPosition().z << std::endl;
+
 }
