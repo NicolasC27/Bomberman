ba7192baebf0bcc5dece1f57ac1d68e1b573d938
nicolas <nicolas1.chevalier@epitech.eu>
2017-06-14 01:23:09 +0200

    start explode and update

--- src/Objects/Bomb.cpp
+++ src/Objects/Bomb.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <Interfaces/AGameObject.hpp>
+#include <OgreParticleSystem.h>
+#include <OgreParticleSystemManager.h>
 #include "Objects/Bomb.hpp"
 
 Bomb::Bomb(AGameObject::Object object) : AGameObject(object, 1)
@@ -15,10 +17,28 @@ Bomb::~Bomb()
 
 }
 
+void 			Bomb::update()
+{
+
+}
+
 void 			Bomb::createEntity()
 {
   _obj = SceneManager->createEntity(getName(), getMeshPrefab());
   dynamic_cast <Ogre::Entity*>(_obj)->setMaterialName(this->getMaterialName());
+}
+
+void			Bomb::Explosion()
+{
+  Ogre::SceneNode	*test;
+// create a particle system named explosions using the explosionTemplate
+  Ogre::ParticleSystem* particleSystem = SceneManager->createParticleSystem("explosions", "explosionTemplate");
+
+// fast forward 1 second  to the point where the particle has been emitted
+  particleSystem->fastForward(1.0);
+
+// attach the particle system to a scene node
+  test->attachObject(particleSystem);
 }
 
 AGameObject::State 	Bomb::getState() const
@@ -52,7 +72,7 @@ double 			Bomb::getPositionY() const
   return 0;
 }
 
-Ogre::SceneManager::PrefabType Bomb::getMeshPrefab() const
+Ogre::SceneManager::PrefabType	Bomb::getMeshPrefab() const
 {
   return Ogre::SceneManager::PT_SPHERE;
 }
